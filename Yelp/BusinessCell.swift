2ea716522da2cd0e00964c2c9fa720f0dabe33f1
Ian Bari <ian.bari@att.net>
2015-05-14 12:04:40 -0700

    Create business cell

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,46 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Ian on 5/13/15.
+//  Copyright (c) 2015 Ian Bari. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var thumbImageView: UIImageView!
+  @IBOutlet weak var distanceLabel: UILabel!
+  @IBOutlet weak var ratingImageView: UIImageView!
+  @IBOutlet weak var reviewsCountLabel: UILabel!
+  @IBOutlet weak var addressLabel: UILabel!
+  @IBOutlet weak var categoriesLabel: UILabel!
+  
+  var business: Business! {
+    didSet {
+      nameLabel.text = business.name
+      thumbImageView.setImageWithURL(business.imageURL)
+      distanceLabel.text = business.distance
+      ratingImageView.setImageWithURL(business.ratingImageURL)
+      reviewsCountLabel.text = "\(business.reviewCount!) Reviews"
+      addressLabel.text = business.address
+      categoriesLabel.text = business.categories
+    }
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+    
+    thumbImageView.layer.cornerRadius = 3
+    thumbImageView.clipsToBounds = true
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,45 +8,65 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
-
-    var businesses: [Business]!
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  var businesses: [Business]!
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-//        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
-//            self.businesses = businesses
-//            
-//            for business in businesses {
-//                println(business.name!)
-//                println(business.address!)
-//            }
-//        })
-        
-        Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
-            
-            for business in businesses {
-                println(business.name!)
-                println(business.address!)
-            }
-        }
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    //        Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
+    //            self.businesses = businesses
+    //
+    //            for business in businesses {
+    //                println(business.name!)
+    //                println(business.address!)
+    //            }
+    //        })
+    
+    Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+      self.businesses = businesses
+      self.tableView.reloadData()
+      
+      for business in businesses {
+        println(business.name!)
+        println(business.address!)
+      }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("BusinessCell", forIndexPath: indexPath) as! BusinessCell
+    
+    cell.business = businesses[indexPath.row]
+    
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if businesses != nil {
+      return businesses.count
+    } else {
+      return 0
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
 }
